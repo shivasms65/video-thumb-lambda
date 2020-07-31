f520c9fe48ee15a68f17cf1d3398ccf222c17faa
Sivakumar Rajendran <siva.rajendran@getdropstream.com>
2020-07-31 12:44:46 +0530

    fixes

--- index.js
+++ index.js
@@ -1,37 +1,104 @@
-const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
-const ffmpeg = require('fluent-ffmpeg');
+// process.env.PATH = process.env.PATH + ':' + process.env['LAMBDA_TASK_ROOT']
 
-ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+const AWS = require('aws-sdk')
+const { spawn, spawnSync } = require('child_process')
+const { createReadStream, createWriteStream } = require('fs')
 
-module.exports = ffmpeg;
+const s3 = new AWS.S3()
+const ffprobePath = '/opt/nodejs/ffprobe'
+const ffmpegPath = '/opt/nodejs/ffmpeg'
+const allowedTypes = ['mov', 'mpg', 'mpeg', 'mp4', 'wmv', 'avi', 'webm']
+const width = process.env.WIDTH
+const height = process.env.HEIGHT
 
-// var ffmpeg = require('fluent-ffmpeg');
-var fs = require('fs');
+module.exports.handler = async (event, context) => {
+  const srcKey = decodeURIComponent(event.Records[0].s3.object.key).replace(/\+/g, ' ')
+  const bucket = event.Records[0].s3.bucket.name
+  const target = s3.getSignedUrl('getObject', { Bucket: bucket, Key: srcKey, Expires: 1000 })
+  let fileType = srcKey.match(/\.\w+$/)
 
-exports.handler = async function(event) {
-  const promise = new Promise(function(resolve, reject) {
-    const file = 'file_example_MOV_480_700kB.mov';
-    ffmpeg(file).format('mov')
-    .on('filenames', function(filenames) {
-      console.log('Will generate ' + filenames.join(', '))
+  if (!fileType) {
+    throw new Error(`invalid file type found for key: ${srcKey}`)
+  }
+
+  fileType = fileType[0].slice(1)
+
+  if (allowedTypes.indexOf(fileType) === -1) {
+    throw new Error(`filetype: ${fileType} is not an allowed type`)
+  }
+
+  function createImage(seek) {
+    return new Promise((resolve, reject) => {
+      let tmpFile = createWriteStream(`/tmp/screenshot.jpg`)
+      const ffmpeg = spawn(ffmpegPath, [
+        '-ss',
+        seek,
+        '-i',
+        target,
+        '-vf',
+        `thumbnail,scale=${width}:${height}`,
+        '-qscale:v',
+        '2',
+        '-frames:v',
+        '1',
+        '-f',
+        'image2',
+        '-c:v',
+        'mjpeg',
+        'pipe:1'
+      ])
+
+      ffmpeg.stdout.pipe(tmpFile)
+
+      ffmpeg.on('close', function(code) {
+        tmpFile.end()
+        resolve()
+      })
+
+      ffmpeg.on('error', function(err) {
+        console.log(err)
+        reject()
+      })
     })
-    .on('end', function() {
-      console.log('Screenshots taken');
-      
-      fs.readdir('/tmp', (err, files) => {
-        files.forEach(file => {
-          console.log(file);
-        });
-      });
-      
-    })
-    .screenshots({
-      timestamps: [0.10],
-      filename: 'thumbnail-at-%s-seconds.png',
-      folder: '/tmp',
-      size: '320x240'
-    });
-  });
+  }
 
-  return promise;
+  function uploadToS3() {
+    return new Promise((resolve, reject) => {
+      let tmpFile = createReadStream(`/tmp/screenshot.jpg`)
+      let dstKey = srcKey.replace(/\.\w+$/, `-thumbnail.jpg`)
+
+      var params = {
+        Bucket: bucket,
+        Key: dstKey,
+        Body: tmpFile,
+        ContentType: `image/jpg`
+      }
+
+      s3.upload(params, function(err, data) {
+        if (err) {
+          console.log(err)
+          reject()
+        }
+        console.log(`successful upload to ${bucket}/${dstKey}`)
+        resolve()
+      })
+    })
+  }
+
+  const ffprobe = spawnSync(ffprobePath, [
+    '-v',
+    'error',
+    '-show_entries',
+    'format=duration',
+    '-of',
+    'default=nw=1:nk=1',
+    target
+  ])
+
+  const duration = Math.ceil(ffprobe.stdout.toString())
+
+  await createImage(duration * 0.25)
+  await uploadToS3()
+
+  return console.log(`processed ${bucket}/${srcKey} successfully`)
 }
